5b79fd32b472ca143434fa0d2f2a51e6eaf97254
Oliver Tonnesen <oliver@tonnesen.com>
2021-01-07 14:18:08 -0800

    Reorganized some code; added stuff for handling key presses.

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import "C"
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"minivim/terminal"
+)
+
+type ExitCode int
+
+const (
+	Break ExitCode = iota
+	Continue
 )
 
 type editorConfig struct {
@@ -15,28 +24,72 @@ type editorConfig struct {
 	rowOff    int
 	colOff    int
 	num_lines int
+	term      terminal.TermConfig
 }
+
+var E editorConfig
 
 func main() {
 	term, err := terminal.Init()
 	if err != nil {
 		log.Panicf("%v\n", err)
 	}
+	E.term = term
 
+	for {
+		refreshScreen()
+		if processKey() == Break {
+			break
+		}
+	}
+	term.Close()
+
+}
+
+// Re-draws the screen to reflect changes in the editor's internal state.
+func refreshScreen() {
 	fmt.Fprintf(os.Stdout, "\x1b[?25l") // Hide cursor
 	fmt.Fprintf(os.Stdout, "\x1b[H")    // Reset cursor position
 
-	for i := 0; i < term.Rows; i++ {
-		fmt.Fprint(os.Stdout, "\x1b[2K") // Clear line
-		fmt.Fprint(os.Stdout, "~")
-		fmt.Fprint(os.Stdout, "\r\n")
+	fmt.Fprintf(os.Stdout, draw())
+
+	fmt.Fprintf(os.Stdout, "\x1b[%d;%dH", E.cx, E.cy) // Move cursor
+	fmt.Fprintf(os.Stdout, "\x1b[?25h")               // Show cursor
+}
+
+// Handles key presses.
+func processKey() ExitCode {
+	c := readKey()
+	switch c {
+	case ctrlKey('q'):
+		return Break
 	}
+	return Continue
+}
 
-	fmt.Fprintf(os.Stdout, "\x1b[%d;%dH", 10, 10) // Move cursor
-	fmt.Fprintf(os.Stdout, "\x1b[?25h")           // Show cursor
+// Returns the byte sequence corresponding to pressing Ctrl + `c`.
+func ctrlKey(c byte) byte {
+	return c & 0x1f
+}
 
-	for {
+// Get next byte of input from stdin
+func readKey() byte {
+	in := bufio.NewReader(os.Stdin)
+	c, err := in.ReadByte()
+	if err != nil {
+		log.Panicf("%v\n", err)
 	}
-	terminal.Close()
+	return c
+}
 
+// Returns file contents as a string, including escape sequences for
+// terminal output.
+func draw() string {
+	var b strings.Builder
+	for i := 0; i < E.term.Rows; i++ {
+		fmt.Fprint(&b, "\x1b[2K") // Clear line
+		fmt.Fprint(&b, "~")
+		fmt.Fprint(&b, "\r\n")
+	}
+	return b.String()
 }
